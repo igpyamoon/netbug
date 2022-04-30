bfd2a377e9ddaeb14b396efe2578ecac1dce27fd
igpyamoon <q_xingjian@163.com>
2022-04-30 13:59:39 +0800

    v0.0.1

---
+++ myturtle.py
@@ -0,0 +1,8 @@
+import turtle
+
+t = turtle.Turtle()
+for i in range (100):
+    t.forward(i)
+    t.left(90)
+
+
---
+++ netbug_test.py
@@ -0,0 +1,33 @@
+import requests
+
+
+# url = "https://www.baidu.com/?query=python"
+# url = "https://fanyi.baidu.com/sug"
+url = "https://movie.douban.com//j/chart/top_list"
+
+param = {
+    "type": "24",
+    "interval_id": "100:90",
+    "action": "",
+    "start": 0,
+    "limit": 20
+}
+
+# s = input("please input the keyword:")
+
+# dat = {
+#     "kw": s
+# }
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"
+}
+
+# resp = requests.get(url, headers=headers)
+# resp = requests.post(url, data=dat)
+resp = requests.get(url, params=param, headers=headers)
+
+print(resp)
+print(resp.request.url)
+print(resp.json())
+
